4a5f2eed191f51eaa152c1134d9fc8b1f39ed0b5
Kodeneko <ladysun.freedom@gmail.com>
2024-12-22 22:05:45 +0100

    [dev] Fix user type

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,9 +1,13 @@
+import { Avatar } from "./Avatar";
+
 interface User {
   id?: string;
   userName: string;
   email: string;
   pass: string;
   salt: string;
+
+  avatarList: Avatar[];
 }
 
 type UserApp = Omit<User, 'pass' | 'salt'>
