a1b8e918a32a69ec809517af880d27c42ee99d66
Kodeneko <ladysun.freedom@gmail.com>
2024-12-23 19:07:21 +0100

    [dev] Fixes model Cat & Elem

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,3 +1,5 @@
+import { Element } from "./Element";
+
 interface Category {
   id?: string;
   name: string;
--- src/model/Element.ts
+++ src/model/Element.ts
@@ -1,14 +1,19 @@
+import { Category } from "./Category";
+
 interface Element {
   id?: string;
   name: string;
   pic: string;
 
   categoryName?: string;
+  category?: Category;
 }
 interface CustomElement {
   id?: string;
-  selected?: Element,
   color: string
+
+  selectedElementName?: string;
+  selectedElement?: Element,
 }
 
 export {
